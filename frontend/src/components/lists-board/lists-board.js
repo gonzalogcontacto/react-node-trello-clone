09e7567c8f81b89d000088d6c70ed19a57d4fe4c
gonzalogcontacto <gonzalogcontacto@gmail.com>
2021-02-11 21:12:02 +0100

    * Add List Form component calling to expected endpoint, then adding the new one list to the UI updatig the state

--- frontend/src/components/lists-board/lists-board.js
+++ frontend/src/components/lists-board/lists-board.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import './lists-board.css';
 import TasksList from './tasks-list'
+import AddListForm from './add-list-form'
 import API from '../../helper/api';
 
 export default function ListsBoard(){
@@ -8,25 +9,34 @@ export default function ListsBoard(){
     const [lists, setLists] = useState([]);
 
     useEffect(() => {
+        //console.log(lists);
         API.lists.getListsOfBoard()
             .then(result => {
                 setLists(result);  
             });
     }, [])
+    
+   
+	const addList = list => {
+        list.id = lists.length + 1
+        API.lists.createList(list)
+        .then(listRes => {
+            setLists([ ...lists, listRes ])
+        });
+	}
 
-        if(!lists) {
-            return <div>Loading...</div>
-        }
 
-        return (
-            <>  
-                <h2>Board para GeeksHubs Academy</h2>
-                <div>
-                    <input></input><button> Añadir lista</button>
-                </div>
-                <ul>
-                    {lists.map(list => <TasksList key={list.id} list={list} /> )}
-                </ul>
-            </>
-        )
+    if(!lists) {
+        return <div>Loading...</div>
+    }
+
+    return (
+        <>  
+            <h2>Board para GeeksHubs Academy</h2>
+            <AddListForm addList={addList}/>
+            <ul>
+                {lists.map(list => <TasksList key={list.id} list={list} /> )}
+            </ul>
+        </>
+    )
 }
--- frontend/src/helper/api.js
+++ frontend/src/helper/api.js
@@ -5,11 +5,29 @@ const API = {
     async getListsOfBoard () {
         const ApiResponse = await fetch(`${baseURL}/list`)
         const json = await ApiResponse.json();
+
+        console.log('getListOfBoard', json)
+
         return json;
     },
     async getTasksOfList (id){
         const ApiResponse = await fetch(`${baseURL}/list/${id}/tasks`)
         const json = await ApiResponse.json();
+        return json;
+    },
+    async createList (list){
+        const ApiResponse = await fetch(`${baseURL}/list`, {
+            method: 'POST', 
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(list)}
+        );
+
+        const json = await ApiResponse.json();
+        console.log('createList', json)
+
         return json;
     }
   },
