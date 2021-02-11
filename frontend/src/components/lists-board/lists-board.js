cbe496d1924daecf90ad1c73424b2510282dc520
gonzalogcontacto <gonzalogcontacto@gmail.com>
2021-02-11 14:04:02 +0100

    * lists board component with more fake darta and basic css to start to see the expected UI

--- frontend/src/components/lists-board/lists-board.js
+++ frontend/src/components/lists-board/lists-board.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import './lists-board.css';
 
 class ListsBoard extends Component {
     constructor() {
         super()
         this.state = {
-            lists: [{"id":1, "name": 'TO DO'}, {"id":2, "name": 'DOING'}]
+            lists: [{"id":1, "name": 'TO DO'}, {"id":2, "name": 'DOING'}, {"id":3, "name": 'DOING'}, {"id":4, "name": 'DOING'}]
         }
     }
 
@@ -13,7 +14,7 @@ class ListsBoard extends Component {
             <>  
                 <h2>Listas del Board por defecto</h2>
                 <ul>
-                    {this.state.lists.map(elm => <div key={elm.id} {...elm}/>)}
+                    {this.state.lists.map(elm => <div key={elm.id} className="list-card"  /> )}
                 </ul>
             </>
         )
